08777470a697fd633180265e6c3ae742278ec9d8
nastyabekesheva <nastya.bekesheva@gmail.com>
2023-09-11 20:05:09 +0300

    added commands list

---
+++ Bekesheva-Sachuk-FI-12/parser.py
@@ -0,0 +1,12 @@
+# TODO: CREATE, INSERT, PRINT_INDEX, SEARCH 
+
+'''     
+        Importing relevant modules
+'''
+import re
+
+
+COMMANDS = ["CREATE", "INSERT", "PRINT_INDEX", "SEARCH"] # we'll be using uppercase to distinguish querry commands
+
+while True: # read user input
+    # do stuff
