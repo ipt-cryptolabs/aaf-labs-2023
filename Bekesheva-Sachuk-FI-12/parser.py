19cf2332e4b2baa2cd6c7fbe511fe808cae6d5a8
nastyabekesheva <nastya.bekesheva@gmail.com>
2023-09-12 23:11:34 +0300

    regex for commands

--- Bekesheva-Sachuk-FI-12/parser.py
+++ Bekesheva-Sachuk-FI-12/parser.py
@@ -7,6 +7,29 @@ import re
 
 
 COMMANDS = ["CREATE", "INSERT", "PRINT_INDEX", "SEARCH"] # we'll be using uppercase to distinguish querry commands
+KEYWORDS = ["WHERE"]
+STOP = ";"
+READ = True
 
+def look_for_commands(string):
+   string = re.sub(r'^\s+', '', string)
+   pattern = r'(?i)(' + '|'.join(COMMANDS) + r')\b'  # (?i) - case-insensitivity, | - pipe for commands
+   match = re.match(pattern, string) 
+   
+   if match:
+       return match.group().upper()  # goup() - for extracting values
+   else:
+       return None  
+
+print(look_for_commands('CREATE jnodfsods;'))
+
+'''
 while True: # read user input
-    # do stuff
+    while READ:
+        user_input = input()
+        COMMAND = look_for_commands(user_input)
+        if COMMAND == "CREATE":
+            #collection_name = re_create()
+        else:
+            print("Incorrect input, plese try again")
+'''
