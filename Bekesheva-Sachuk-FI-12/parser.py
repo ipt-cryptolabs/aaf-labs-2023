3597cf5bbad8ec56fd20d6896995be854e076b2f
nastyabekesheva <nastya.bekesheva@gmail.com>
2023-09-13 00:08:25 +0300

    added parser for CREATE

--- Bekesheva-Sachuk-FI-12/parser.py
+++ Bekesheva-Sachuk-FI-12/parser.py
@@ -12,16 +12,35 @@ STOP = ";"
 READ = True
 
 def look_for_commands(string):
-   string = re.sub(r'^\s+', '', string)
+   string = re.sub(r'^\s*', '', string)
    pattern = r'(?i)(' + '|'.join(COMMANDS) + r')\b'  # (?i) - case-insensitivity, | - pipe for commands
    match = re.match(pattern, string) 
-   
-   if match:
-       return match.group().upper()  # goup() - for extracting values
-   else:
-       return None  
 
-print(look_for_commands('CREATE jnodfsods;'))
+
+   if match:
+        COMMAND = match.group().upper()
+        string = re.sub(f'(?i){COMMAND}' + r'\s+', '', string)
+        return COMMAND, string  # goup() - for extracting values
+   else:
+       return None, None  
+   
+
+def re_create(string):
+    pattern = '^[a-zA-Z]*[a-zA-Z0-9_]*\s*;'
+    match = re.search(pattern, string)
+    if match:
+        string = re.sub(';', '', match.group())
+        return string
+    else:
+        return None
+
+
+
+s = '    CREaTE      jnodfsods;'
+c, s = look_for_commands(s)
+print(c)
+n = re_create(s)
+print(n)
 
 '''
 while True: # read user input
