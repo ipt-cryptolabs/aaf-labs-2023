aff83c237ca0bd796669ba2d9e1544a3b772a97f
nastyabekesheva <nastya.bekesheva@gmail.com>
2023-09-18 21:52:12 +0300

    regex fixed

--- Bekesheva-Sachuk-FI-12/parser.py
+++ Bekesheva-Sachuk-FI-12/parser.py
@@ -19,13 +19,13 @@ def look_for_commands(string):
    if match:
         COMMAND = match.group(1).upper()
         string = re.sub(f'(?i){COMMAND}' + r'\s+', '', string)
-        return COMMAND, string  # goup() - for extracting values
+        return COMMAND, string  
    else:
        return None, None  
    
 
 def re_create(string):
-    pattern = '^[a-zA-Z]*[a-zA-Z0-9_]*\s*;'
+    pattern = '^[a-zA-Z]*[a-zA-Z0-9_]*\s*;'  # search for collection name, nvm spaces
     match = re.search(pattern, string)
     if match:
         string = re.sub(';', '', match.group())
@@ -34,17 +34,18 @@ def re_create(string):
         return None
 
 def re_insert(string):
-    pattern = '^([a-zA-Z]*[a-zA-Z0-9_]*)\s*"([a-zA-Z]*\s*[a-zA-Z0-9_]*)"\s*;' # я оце пишу ці регулярки, а хто потім згадає як ця хуйня мені в голову прийшла
+    pattern = '^([a-zA-Z]*[a-zA-Z0-9_]*)\s*"(.*)"\s*;'  # search for collection name, nvm spaces, search for value (must be in " ")
     match = re.match(pattern, string) 
     if match:
         collection_name = match.group(1)
         value = match.group(2)
+        print(collection_name, value)
         return collection_name, value
     else:
         return None, None
 
 def re_print(string):
-    pattern = '^[a-zA-Z]*[a-zA-Z0-9_]*\s*;'
+    pattern = '^[a-zA-Z]*[a-zA-Z0-9_]*\s*;'  # search for collection name, nvm spaces
     match = re.search(pattern, string)
     if match:
         string = re.sub(';', '', match.group())
@@ -53,29 +54,50 @@ def re_print(string):
         return None
     
 def re_search(string):
-    pattern = '^([a-zA-Z]*[a-zA-Z0-9_]*)\s*(?i:WHERE\s*"([a-zA-Z]*\s*[a-zA-Z0-9_]*)"\s*)?\s*;'
+    pattern = '^([a-zA-Z]*[a-zA-Z0-9_]*)\s*(?i:WHERE\s*"(.*)"\s*)?\s*;'  # search for collection name, nvm spaces, group keyword if present
     match = re.search(pattern, string)
     if match:
         collection_name = match.group(1)
-        print(collection_name)
         if match.group(1):
-            keyword = match.group(1)
+            keyword = match.group(2)
             return collection_name, keyword
-    else:
-        return None
-
-s = '\tCREaTE      jnodfs ;'
-c, s = look_for_commands(s)
-n = re_create(s)
-re_insert(s)
-re_search(s)
-'''
-while True: # read user input
-    while READ:
-        user_input = input()
-        COMMAND = look_for_commands(user_input)
-        if COMMAND == "CREATE":
-            #collection_name = re_create()
         else:
-            print("Incorrect input, plese try again")
-'''
+            return collection_name, None
+    else:
+        return None, None
+
+while READ:
+    user_input = input()
+    COMMAND, STRING = look_for_commands(user_input)
+    if user_input == "--s":
+        READ = False
+    elif COMMAND == "CREATE":
+        collection_name = re_create(STRING)
+        if collection_name:
+            print(f'creating collection \'{collection_name}\'...')
+        else:
+            print("incorrect collection_name")
+    elif COMMAND == "INSERT":
+        collection_name, value = re_insert(STRING)
+        if collection_name and value:
+            print(f'inserting \'{value}\' into collection \'{collection_name}\'...')
+        else:
+            print("incorrect collection_name or value")
+    elif COMMAND == "PRINT_INDEX":
+        collection_name = re_print(STRING)
+        if collection_name:
+            print(f'printing collection \'{collection_name}\' as index...')
+        else:
+            print("incorrect collection_name")
+    elif COMMAND == "SEARCH":
+        collection_name, keyword = re_search(STRING)
+        if collection_name:
+            if keyword:
+                print(f'searching collection \'{collection_name} with keyword \'{keyword}\'...')
+            else:
+                print(f'searching collection \'{collection_name}\'...')
+        else:
+            print("incorrect collection_name or no keyword provided")
+    else:
+        print("incorrect input, plese try again")
+
