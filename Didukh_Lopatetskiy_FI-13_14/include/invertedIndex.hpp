48bf31685dd85d2ef108a69bdb2bd3c54d9b4001
maksymdidukh <maxym.didukh@logivations.com>
2023-09-25 20:07:38 +0200

    implement first working realisation if parser (obly one command so far)

--- Didukh_Lopatetskiy_FI-13_14/include/invertedIndex.hpp
+++ Didukh_Lopatetskiy_FI-13_14/include/invertedIndex.hpp
@@ -1,9 +1,42 @@
 #pragma once
 
 #include <iostream>
+#include <string>
+#include <vector>
+#include <string>
+#include <map>
+#include <regex>
 
-class pruvit
-{
+// namespace {
+//     std::regex insertPattern("^INSERT\\s+([a-zA-Z_][a-zA-Z0-9_]*)\\s+\\{(.+?)\\};$");
+//     std::regex insertRegex("INSERT", std::regex_constants::icase);
+//     std::regex createRegex("CREATE", std::regex_constants::icase);
+//     std::regex printIndexRegex("PRINT_INDEX", std::regex_constants::icase);
+//     std::regex containsRegex("CONTAINS", std::regex_constants::icase);
+//     std::regex searchRegex("SEARCH", std::regex_constants::icase);
+// }
+
+class Parser {
+private:
+    std::vector<std::string> tokens;
+
+private:
+    std::string mergeTokens(const std::vector<std::string>& tokens);
+
 public:
-    void say_hello();
+    std::string lexer(std::string inputString);
+
+};
+
+
+class InvertedIndex {
+private: 
+    std::map<int, std::vector<int>> invertedIndex;
+public: 
+    // type are definitely wrong)
+    void insert();
+    void create();
+    void print_index();
+    void contains();
+    void search();
 };
--- Didukh_Lopatetskiy_FI-13_14/src/invertedIndex.cpp
+++ Didukh_Lopatetskiy_FI-13_14/src/invertedIndex.cpp
@@ -1,5 +1,136 @@
 #include "invertedIndex.hpp"
 
-void pruvit::say_hello() {
-    std::cout << "hello" << std::endl;
+// std::string Parser::mergeTokens(const std::vector<std::string>& tokens) {
+//     std::string res;
+
+//     for(const auto& token : tokens)
+//         res += token + " ";
+
+//     return res;
+// }
+
+std::string Parser::lexer(std::string inputString) {
+    std::regex insertPattern("^\\s*INSERT\\s+([a-zA-Z][a-zA-Z0-9_]*)\\s+\\{(.+?)\\};\\s*$", std::regex::icase);
+    std::smatch match;
+
+    if (std::regex_match(inputString, match, insertPattern)) {
+        std::string collectionName = match[1];
+        std::string values = match[2];
+        
+        std::regex identifierPattern("[a-zA-Z][a-zA-Z0-9_]*");
+        if (std::regex_match(collectionName, identifierPattern)) {
+            std::cout << "Collection Name: " << collectionName << std::endl;
+            std::cout << "Values: " << values << std::endl << std::endl;
+        } else {
+            std::cout << "Invalid collection name." << std::endl << std::endl;
+        }
+    } else {
+        std::cout << "Invalid command." << std::endl << std::endl;
+    }
+
+    return inputString;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void InvertedIndex::insert() {
+    std::cout << "InvertedIndex::insert() called\n";
+}
+
+void InvertedIndex::create() {
+    std::cout << "InvertedIndex::create() called\n";
+    
+}
+
+void InvertedIndex::print_index() {
+    std::cout << "InvertedIndex::print_index() called\n";
+    
+}
+
+void InvertedIndex::contains() {
+    std::cout << "InvertedIndex::contains() called\n";
+    
+}
+
+void InvertedIndex::search() {
+    std::cout << "InvertedIndex::search() called\n";
+    
 }
