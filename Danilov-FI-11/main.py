1526e564332ab04baa5a8f485ae7479b4ec0fc04
Kostia9 <danilov.k11@gmail.com>
2023-10-04 11:32:46 +0300

    Some changes

--- Danilov-FI-11/main.py
+++ Danilov-FI-11/main.py
@@ -4,8 +4,7 @@ from CommandHandler import handle_create, handle_insert, handle_select
 
 buffer = []
 while True:
-    cmd = input()
-
+    cmd = input()       
     if cmd.strip().lower() == 'exit':
         break
 
@@ -14,27 +13,32 @@ while True:
     if ';' in cmd:
         full_cmd = ' '.join(buffer).strip()
 
-        command_handled = False
-
         if full_cmd.upper().startswith("CREATE"):
             parsed_command = parse_create(full_cmd)
             if parsed_command["success"]:
                 handle_create(parsed_command)
-                command_handled = True
+            else:
+                print("Incorrect syntax for CREATE. Correct syntax is CREATE table_name (column_name [INDEXED] [, ...]);")
 
         elif full_cmd.upper().startswith("INSERT"):
             parsed_command = parse_insert(full_cmd)
             if parsed_command["success"]:
                 handle_insert(parsed_command)
-                command_handled = True
+            else:
+                print("Incorrect syntax for INSERT. Correct syntax is INSERT [INTO] table_name (“value” [, ...]);")
 
         elif full_cmd.upper().startswith("SELECT"):
             parsed_command = parse_select(full_cmd)
             if parsed_command["success"]:
                 handle_select(parsed_command)
-                command_handled = True
-
-        if not command_handled:
+            else:
+                print("""Incorrect syntax for SELECT. Correct syntax is 
+                    SELECT [agg_function(agg_column) [, ... ]]
+                     FROM table_name
+                     [WHERE condition]
+                     [GROUP_BY column_name [, ...] ];""")
+        else:
             print("Error: Unknown or incorrect command")
 
         buffer = []
+
