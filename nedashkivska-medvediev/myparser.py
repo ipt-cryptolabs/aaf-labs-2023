012c80bb7f8e8af632366ad5d9b6367f2076886b
Bohelgi <olegmedv-ipt25@lll.kpi.ua>
2023-10-08 20:50:45 +0300

    added parser

---
+++ nedashkivska-medvediev/myparser.py
@@ -0,0 +1,40 @@
+from pyparsing import *
+
+
+CREATE = Keyword("CREATE")
+INSERT_INTO = Keyword("INSERT INTO")
+SELECT = Keyword("SELECT")
+FROM = Keyword("FROM")
+WHERE = Keyword("WHERE")
+EXIT = Keyword("exit")
+SEMICOLON = Suppress(";")
+COMMA = Suppress(",")
+
+
+identifier = Word(alphas + "_")
+table_name = identifier("table_name")
+column_name = identifier("column_name")
+
+
+value = QuotedString('"')
+condition = column_name + Suppress("<") + value("condition")
+data = value + ZeroOrMore(COMMA + value)
+
+
+create_command = CREATE + table_name + Suppress("(") + column_name + ZeroOrMore(COMMA + column_name) + Suppress(")") + SEMICOLON
+insert_command = INSERT_INTO + table_name + Suppress("(") + data + Suppress(")") + SEMICOLON
+select_all_command = SELECT + "*" + FROM + table_name + SEMICOLON
+select_with_where_command = SELECT + FROM + table_name + WHERE + condition + SEMICOLON
+exit_command = EXIT + SEMICOLON
+
+
+command = create_command | insert_command | select_all_command | select_with_where_command | exit_command
+
+def parse(a: str) -> list:
+    res = command.parseString(a)
+    return res.asList()
+
+
+input_text = "CREATE Persons (PersonID, LastName, FirstName, Address, City);"
+parsed_data = parse(input_text)
+print(parsed_data)
