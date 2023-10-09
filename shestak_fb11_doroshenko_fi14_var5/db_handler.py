9dfc2bfac2aa1f2cef3471d3b74dbfb98cd223b0
Max Shestak <ser_max778@ukr.net>
2023-10-09 16:21:02 +0300

    Added where select functional.

--- shestak_fb11_doroshenko_fi14_var5/db_handler.py
+++ shestak_fb11_doroshenko_fi14_var5/db_handler.py
@@ -27,7 +27,37 @@ class DB_Handler:
         with open(self.path, 'r', encoding='UTF-8') as f:
             table = dict(json.load(f))
             if column:
-                pass
+                keys = list(table[table_name].keys())
+                exceptions = []
+
+                for c in table[table_name].keys():
+                    for e, i in enumerate(table[table_name][c]):
+                        if c == column:
+                            if cond == '=':
+                                if i != value:
+                                    exceptions.append(e)
+                            elif cond == '>':
+                                if i <= value:
+                                    exceptions.append(e)
+                            elif cond == '<':
+                                if i >= value:
+                                    exceptions.append(e)
+
+                values = []
+
+                for c in table[table_name].values():
+                    items = []
+                    for e, i in enumerate(c):
+                        if e not in exceptions:
+                            items.append(i)
+                    values.append(items)
+
+                print(' '.join(keys))
+                for i in range(len(values[0])):
+                    for n in range(len(values)):
+                        print(values[n][i], end=' ')
+                    print()
+
             else:
                 keys = list(table[table_name].keys())
                 values = [v for v in table[table_name].values()]
--- shestak_fb11_doroshenko_fi14_var5/main.py
+++ shestak_fb11_doroshenko_fi14_var5/main.py
@@ -14,8 +14,18 @@ import db_handler
 
 handler = db_handler.DB_Handler('db.json')
 handler.create('Students', ['name', 'age'])
-handler.create('Cats', ['name', 'age'])
+handler.create('Cats', ['name', 'age', 'type'])
+
 handler.insert('Students', ['Yura', 19])
 handler.insert('Students', ['Max', 19])
-# handler.insert('1', [])
+handler.insert('Students', ['Bob', 24])
 
+handler.insert('Cats', ['A', 1, 9])
+handler.insert('Cats', ['B', 2, 8])
+handler.insert('Cats', ['C', 3, 7])
+handler.insert('Cats', ['D', 4, 6])
+
+# handler.select('Students')
+# handler.select('Cats')
+
+handler.select('Cats', column='name', cond='=', value='D')
