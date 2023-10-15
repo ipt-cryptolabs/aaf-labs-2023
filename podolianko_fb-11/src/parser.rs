ad672019cc79b8a886367453f46578eec6bc2175
timofey282228 <tymopod-ipt25@lll.kpi.ua>
2023-10-16 00:00:51 +0300

    new parser base types

--- podolianko_fb-11/src/parser.rs
+++ podolianko_fb-11/src/parser.rs
@@ -1,21 +1,33 @@
-pub enum Token {
-    Command(Command),
-    SetName(String),
-    LineSegment(i64, i64),
-    Point(i64),
-    WhereClause(Option<WhereClause>),
+#[derive(Debug)]
+struct Point(i64);
+
+#[derive(Debug)]
+struct LineSegment(i64, i64);
+
+#[derive(Debug)]
+enum Command {
+    Create(String),
+    Insert {
+        set_name: String,
+        line_segment: LineSegment,
+    },
+    PrintTree {
+        set_name: String
+    },
+    Contains{
+        set_name: String,
+        line_segment: LineSegment,
+    },
+    Search{
+        set_name: String,
+        where_query: Option<WhereClause>
+    }
 }
 
-pub enum Command {
-    Create,
-    Insert,
-    PrintTree,
-    Contains,
-    Search,
+#[derive(Debug)]
+enum WhereClause{
+    ContainedBy(LineSegment),
+    Intersects(LineSegment),
+    RightOf(Point)
 }
 
-pub enum WhereClause {
-    ContainedBy,
-    Intersects,
-    RightOf,
-}
