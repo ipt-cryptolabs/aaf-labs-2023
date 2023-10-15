32f753d6455bb9ae7a6b009aac77364fe07b0fc9
timofey282228 <tymopod-ipt25@lll.kpi.ua>
2023-10-16 01:24:52 +0300

    fix: explicitly passing newline to lexer as it gets removed by Lines

--- podolianko_fb-11/src/cli.rs
+++ podolianko_fb-11/src/cli.rs
@@ -1,8 +1,9 @@
 use crate::lexer;
+use crate::lexer::{Lexer, LexingError, LexingState};
 use std::error::Error;
 use std::io;
-use std::io::{BufRead, stdout};
 use std::io::Write;
+use std::io::{stdout, BufRead};
 
 pub struct CLI;
 const PROMPT: &str = " > ";
@@ -13,7 +14,7 @@ impl CLI {
         CLI
     }
 
-    fn prompt(prompt_text: &str) -> io::Result<()>{
+    fn prompt(prompt_text: &str) -> io::Result<()> {
         print!("{}", prompt_text);
         stdout().flush()
     }
@@ -23,22 +24,37 @@ impl CLI {
         let locked_stdin = io::stdin().lock();
         let mut lexer = lexer::Lexer::new();
 
-
         for line in locked_stdin.lines() {
             println!("Line: {:?}", line);
             match lexer.tokenize(&line?) {
                 Ok(state) => {
                     if let lexer::LexingState::End = state {
-                        // todo!(); // send to parser, reset lexer
+                        // todo!(); // send to parser
                         let lexed_input = lexer.collect();
                         println!("{:?}", lexed_input);
                         Self::prompt(PROMPT)?;
                     } else {
+                        // This marks an explicit whitespace later used by the parser.
+                        // Would not need it if Lines preserved '\n'.
+                        match lexer.tokenize("\n") {
+                            Ok(LexingState::Continue) => {
+                                // expected
+                            }
+                            Ok(LexingState::End) => {
+                                // lexer is broken?
+                                eprintln!("Internal Lexer error");
+                                return Ok(()); // TODO implement a valid convertible Error
+                            }
+                            Err(err) => {
+                                eprintln!("Lexer errored: {:?}", err);
+                            }
+                        }
                         Self::prompt(PROMPT_CONT)?;
                         continue;
                     }
                 }
                 Err(err) => {
+                    // TODO implement a valid convertible Error yet
                     eprintln!("Lexer errored: {:?}", err);
                 }
             }
