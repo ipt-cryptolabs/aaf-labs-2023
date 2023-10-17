13c1ce17926e5ee81bdeec8a299a2a6ec74ea152
Volodymyr Herashchenko <cholkegn@gmail.com>
2023-10-17 08:50:26 +0300

    moved to main.py

--- Orlov(FB-14)-Heraschenko(FI-14)/aparser.py
+++ Orlov(FB-14)-Heraschenko(FI-14)/aparser.py
@@ -29,38 +29,6 @@ def parseString(input_str: str):
     input_str = input_str.strip()
     output = []
 
-    bracket_count = 0
-    parentheses_count = 0
-    quote_open = False
-
-    for char in input_str:
-            if char == '(':
-                parentheses_count += 1
-            elif char == ')':
-                parentheses_count -= 1
-            elif char == '[':
-                bracket_count += 1
-            elif char == ']':
-                bracket_count -= 1
-            elif char == '"':
-                quote_open = not quote_open
-
-    while not (input_str.endswith(';') and bracket_count == 0 and parentheses_count == 0 and not quote_open):
-        # Continuously read input
-        new_input = input('> ')
-        input_str += ' ' + new_input
-
-        for char in new_input:
-            if char == '(':
-                parentheses_count += 1
-            elif char == ')':
-                parentheses_count -= 1
-            elif char == '[':
-                bracket_count += 1
-            elif char == ']':
-                bracket_count -= 1
-            elif char == '"':
-                quote_open = not quote_open
     #case if command is CREATE
     #return array = ["command", "table name", "array of pairs: (column, isIndexed)",]
     if(re.match(regForCreate, input_str) != None):  
--- Orlov(FB-14)-Heraschenko(FI-14)/main.py
+++ Orlov(FB-14)-Heraschenko(FI-14)/main.py
@@ -4,8 +4,42 @@ if __name__ == "__main__":
     output = []
     while True:
         print("Type the command(SQL-like)")
-        ins = input()
-        output = aparser.parseString(ins)
+        
+        input_str = input()
+        bracket_count = 0
+        parentheses_count = 0
+        quote_open = False
+
+        for char in input_str:
+                if char == '(':
+                    parentheses_count += 1
+                elif char == ')':
+                    parentheses_count -= 1
+                elif char == '[':
+                    bracket_count += 1
+                elif char == ']':
+                    bracket_count -= 1
+                elif char == '"':
+                    quote_open = not quote_open
+
+        while not (input_str.endswith(';') and bracket_count == 0 and parentheses_count == 0 and not quote_open):
+            # Continuously read input
+            new_input = input('> ')
+            input_str += ' ' + new_input
+
+            for char in new_input:
+                if char == '(':
+                    parentheses_count += 1
+                elif char == ')':
+                    parentheses_count -= 1
+                elif char == '[':
+                    bracket_count += 1
+                elif char == ']':
+                    bracket_count -= 1
+                elif char == '"':
+                    quote_open = not quote_open
+            
+        output = aparser.parseString(input_str)
         if(output[0] == -1):
             break
-   
+            
