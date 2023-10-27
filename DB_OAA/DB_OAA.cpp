c6b504da36ed423f824fc9af36654f9b2f46be5f
Patsora-TatarynovaFI-13 <147597698+Patsora-TatarynovaFI-13@users.noreply.github.com>
2023-10-27 13:55:11 +0300

    Patsora(FI-13), Tatarynova(FI-13)

---
+++ DB_OAA/DB_OAA.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include "Table.hpp"
+#include "Parser.hpp"
+
+using namespace std;
+
+int main() {
+	Table test1("C:\\Users\\Acer\\Downloads\\test_join1.csv");
+	Table test2("C:\\Users\\Acer\\Downloads\\test_join2.csv");
+	test1.join(test2, "owner_ID");
+	test1.toCSV("C:\\Users\\Acer\\Downloads\\test1.csv");
+	test1.select();
+}
