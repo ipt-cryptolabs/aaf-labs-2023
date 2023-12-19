3e781a3b8b297b6832b43a7f03ffeee11259bb89
maksymdidukh <maxym.didukh@logivations.com>
2023-12-19 20:42:07 +0100

    add intersection finding

--- Didukh_Lopatetskiy_FI-13_14/include/invertedIndex.hpp
+++ Didukh_Lopatetskiy_FI-13_14/include/invertedIndex.hpp
@@ -4,17 +4,19 @@
 #include <vector>
 #include <string>
 #include <map>
+#include <iterator>
 #include <set>
 
 #include "parser.hpp"
 
 class Collection {
 private: 
-    std::map<int, std::vector<std::string>> invertedIndex;
+    std::map<int, std::set<std::string>> invertedIndex;
     std::vector<std::set<int>> sets;
 
 public:
-    void insert(const std::set<int>& set);
+    void insert(const std::set<int> &set);
+    bool contains(const std::set<int> &set);
     void print_index();
     std::vector<std::set<int>> getSets() { return sets; }
 };
@@ -25,14 +27,14 @@ private:
     Parser parser;
 
 private:    
-    std::set<int> getSetFromTokens(const std::vector<std::string>& tokens);
+    std::set<int> getSetFromTokens(const std::vector<std::string> &tokens);
 
 public:
-    void insertSet(const std::string& collecntionName, const std::set<int>& set);
-    bool containsCollection(const std::string& collectionName, const std::set<int>& set);
-    void createCollection(const std::string& collecntionName);
-    std::vector<std::set<int>> searchInCollection(const std::string& collectionName);
-    void printCollectionIndex(const std::string& collectionName);
+    void insertSet(const std::string &collecntionName, const std::set<int> &set);
+    bool containsCollection(const std::string &collectionName, const std::set<int> &set);
+    void createCollection(const std::string &collecntionName);
+    std::vector<std::set<int>> searchInCollection(const std::string &collectionName);
+    void printCollectionIndex(const std::string &collectionName);
 
-    void parse(const std::string& inputString);
+    void parse(const std::string &inputString);
 };
