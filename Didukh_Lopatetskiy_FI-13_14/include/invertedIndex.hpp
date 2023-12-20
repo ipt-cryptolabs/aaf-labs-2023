0377385665346f8d5593993f03747f727dd87058
maksymdidukh <maxym.didukh@logivations.com>
2023-12-20 17:02:33 +0100

    refactroing

--- Didukh_Lopatetskiy_FI-13_14/include/invertedIndex.hpp
+++ Didukh_Lopatetskiy_FI-13_14/include/invertedIndex.hpp
@@ -31,15 +31,15 @@ private:
     void printsSets(const std::vector<std::set<int>> &sets);
 
 public:
-    void insertSet(const std::string &collecntionName, const std::set<int> &set);
-    bool containsCollection(const std::string &collectionName, const std::set<int> &set);
+    void parse(const std::string &inputString);
+
     void createCollection(const std::string &collecntionName);
+    void insertSet(const std::string &collecntionName, const std::set<int> &set);
     void printCollectionIndex(const std::string &collectionName);
+    bool containsCollection(const std::string &collectionName, const std::set<int> &set);
    
     std::vector<std::set<int>> searchInCollection(const std::string &collectionName);
     std::vector<std::set<int>>  intersectsSearch(const std::string &collecntionName, const std::set<int> &set);
     std::vector<std::set<int>>  containsSearch(const std::string &collecntionName, const std::set<int> &set);
     std::vector<std::set<int>>  containedBySearch(const std::string &collecntionName, const std::set<int> &set);
-
-    void parse(const std::string &inputString);
 };
