0a1019121a0d3a1e5f99ddeb771459c0df2de033
Eugene <djeka242@gmail.com>
2023-12-30 22:51:47 +0200

    refactored Trie.h

--- Melnyk_Bobrov_FI-13-11/include/Trie.h
+++ Melnyk_Bobrov_FI-13-11/include/Trie.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <string_view>
 #include <iostream>
+#include <regex>
 
 #include "Types.h"
 
@@ -19,7 +20,8 @@ public:
     void insert(const std::string_view& word) noexcept;
     bool search(const std::string_view& word) const noexcept;
     void print() const noexcept;
-    std::vector<std::string> allStrings() const noexcept;
+    void match(const std::string& pattern, const std::string& order) const noexcept;
+    std::vector<std::string> allStrings(const std::string from, const std::string to) const noexcept;
 
 private:
     struct Node 
@@ -46,18 +48,60 @@ private:
             }
         }
 
-        void allStrings(std::string word, std::vector<std::string>& words) const noexcept
+    void allStrings(const std::string& from, const std::string& to, const std::string& word, std::vector<std::string>& words) const noexcept
+    {
+        if(m_isLeaf)
         {
-            if(m_isLeaf)
+            if( ( (word.compare(to) <= 0) || (to == "")) && ((word.compare(from) >= 0) || (from == "")) )
             {
-                words.push_back(word + m_data);
-            }
-
-            for(const auto& it : m_children)
-            {
-                it.allStrings(word + m_data, words);
+                words.push_back(word);
             }
         }
+    
+        for(const auto& it : m_children)
+        {   
+            it.allStrings(from, to, word + it.m_data, words);
+        }
+    }
+
+    void match(int position, std::string word, std::vector<std::string>& words, const std::string& pattern) const noexcept
+    {
+        if(position > pattern.size())
+            return;
+
+        if(m_isLeaf && (position == pattern.size() || pattern[position] == '*'))
+        {
+            words.push_back(word);
+        }
+
+        if(pattern[position] == '?')
+        {
+            for(const auto& it : m_children)
+            {
+                it.match(position + 1, word + it.m_data, words, pattern);
+            }
+        }
+        else if(pattern[position] == '*')
+        {
+            for(const auto& it : m_children)
+            {
+                it.match(position, word + it.m_data, words, pattern);
+            }
+        }
+        else
+        {
+            auto iter = std::find_if(m_children.begin(), m_children.end(), [&](const auto& node)
+            {
+                return node.m_data == pattern[position];  
+            });
+
+            if(iter != m_children.end())
+            {
+                iter->match(position + 1, word + iter->m_data, words, pattern);
+            }
+        }
+    }
+
 
         /* members */
         char m_data;
