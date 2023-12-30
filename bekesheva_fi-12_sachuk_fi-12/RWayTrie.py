043ebb47a2c0b09148e55fa78b12f9aee4176e3b
Nastya Bekesheva <40558678+nastyabekesheva@users.noreply.github.com>
2023-12-30 23:11:11 +0200

    [CP-1] A. Bekesheva (FI-12), A. Sachuk (FI-12) (#26)
    * initial commit
    
    * started working on parser
    
    * added .gitignore
    
    * added commands list
    
    * regex for commands
    
    * added parser for CREATE
    
    * added parser for INSERT
    
    * added parser for PRINT_INDEX
    
    * added parser for SEARCH
    
    * parser fixed | trie created
    
    * regex for SEARCH added | main body added
    
    * regex fixed
    
    * regex fixed
    
    * cleaned
    
    * regex fixed
    
    * added basic functions
    
    * fixed delete command
    
    * overloaded print
    
    * fixed insert\
    
    * created db
    
    * fixed insert | added search
    
    * added get_all_values command
    
    * git ignore added
    
    * added DB to main
    
    * fixed search
    
    * updated parser | search fixed
    
    * updated print
    
    * fixed delete
    
    * search fixed
    
    * .
    
    * fixed main
    
    * fixed search output
    
    * fixed search variations
    
    * fixed collection crete
    
    * main part
    
    * additional part
    
    * fixed dirs
    
    * fixed dirs

---
+++ bekesheva_fi-12_sachuk_fi-12/RWayTrie.py
@@ -0,0 +1,147 @@
+class Node:
+    def __init__(self, key=" ", children={}, value=[]):
+        self._key = key
+        self._children = children # map key - value --{"key": next nodes letters}-- 
+        self._value = value # non-None if it's the last letter in word, {}
+
+    def __eq__(self, value: object) -> bool:
+        return self._key == value._key and self._children == value._children and self._value == value._value 
+
+    def __repr__(self) -> str: 
+        if self._value:
+            return f'{self._key}\n{self._value}'
+        else:
+            return str(self._key)
+    
+    
+class Trie:
+    def __init__(self) -> None:
+        self._root = Node(key="", children={}, value=[]) # root is always empty
+
+    def insert(self, key: str, value: dict) -> None:
+        self.__insert__(self._root, key, value)
+
+    def __insert__(self, x: Node, key: str, value: dict) -> None:
+        for i in range(len(key)):
+            if key[i] not in x._children.keys():
+                x._children[key[i]] = Node(key[i], children={},  value = [])
+            x = x._children[key[i]]
+
+        x._value.append(value)
+
+    def search(self, key1: str, prefix = False) -> list:
+        if prefix == False:
+            return self.__search__(self._root, key1)
+        else:
+            return self.__search_prefix__(self._root, key1)
+
+    def __search__(self, x: Node, key: str) -> Node:
+        for i in range(len(key)):
+            if key[i].lower() not in x._children:
+                return None
+            
+            x = x._children[key[i]]
+
+        return x
+
+    def __search_prefix__(self, x: Node, key: str) -> list:
+        matches = []
+        nodes_stack = [(x, key, 0)]
+
+        while nodes_stack:
+            x, sub_key, i = nodes_stack.pop()
+            while i < len(sub_key):
+                if sub_key[i].lower() not in x._children:
+                    break  # Key not found, so break out of the loop
+                x = x._children[sub_key[i].lower()]
+                i += 1
+
+            if i == len(sub_key):
+                self.__collect_matches__(x, matches)
+            nodes_stack.extend((child, sub_key, i) for child in x._children.values())
+
+        return matches
+
+    def __collect_matches__(self, x: Node, matches: list) -> None:
+        if x._value:
+            matches.append(x)  # Add the current node to the list of matches
+
+
+    
+    def delete(self, key: str) -> None:
+        self.__delete__(self._root, key)
+
+
+    def __delete__(self, x: Node, key: str) -> None:
+        nodes_stack = [] 
+
+        for i in range(len(key)):
+            if key[i] not in x._children:
+                return  # key not found, nothing to delete
+            x = x._children[key[i]]
+            nodes_stack.append(x)
+       
+        if all(child._value is None for child in x._children.values()): # mark the node as deleted only if it has no children
+            x._value = None
+        else:
+            x._value = []
+
+        while nodes_stack:
+            x = nodes_stack.pop()
+            if all(child._value is None for child in x._children.values()): # check if there are no children
+                if nodes_stack:
+                    parent = nodes_stack[-1]
+                    del parent._children[x._key]
+
+    def __repr__(self) -> str:
+        return self.__display__()
+
+    def __display__(self) -> str:
+        res = ""
+        stack = [(self._root, "")]
+        while stack:
+            x, prefix = stack.pop()
+            if x._value:
+                res += f"{prefix} - {x._value}\n"
+                print(x._value)
+            for child in x._children.values():
+                stack.append((child, prefix + child._key))
+        return res
+    
+    def get_all_values(self) -> list:
+        return self.__get_all_values__(self._root)
+    
+    def __get_all_values__(self, x: Node):
+        values = []
+        nodes_stack = [x]
+    
+        while nodes_stack:
+            x = nodes_stack.pop()
+            if x._value != [] and x._value != None:
+                values.append(x._value)
+            for child in x._children.values():
+                nodes_stack.append(child)
+        return values
+    
+    def get_words(self) -> list:
+        return self.__get_words__(self._root)
+    
+    def __get_words__(self, x: Node):
+        res = {}
+        stack = [(self._root, "")]
+        while stack:
+            x, prefix = stack.pop()
+            if x._value:
+                #res += f"{prefix} - {x._value}\n"
+                for dic in x._value:
+                    for key, value in dic.items():
+                        try:
+                            res[key].append(prefix)
+                        except KeyError:
+                            res.update({key: [prefix]})
+            for child in x._children.values():
+                stack.append((child, prefix + child._key))
+        return res
+
+
+ 
