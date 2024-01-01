e126fa316cdbc6ec47f0eb58d53d75b36ccfe0d5
Viktoria <96387123+tOrryV@users.noreply.github.com>
2024-01-01 20:55:58 +0200

    [V-1] Balatska (FI-13), Krivoshapova (FI-13) (#2)

---
+++ balatska_fi-13_krivoshapova_fi-13_cp1/src/main.cpp
@@ -0,0 +1,56 @@
+#include "functions.h"
+#include "parser.h"
+#include <iostream>
+#include <sstream>
+#include <string>
+
+int main() {
+    std::string input;
+    SelectionResult result;
+    std::vector<Command> collection;
+
+
+    while (true) {
+        std::cout << "Enter a command (or 'exit' to quit): ";
+        std::getline(std::cin, input);
+
+        if (input == "exit") {
+            break;
+        }
+
+        Command command = parseCommand(input);
+
+        switch (command.type) {
+        case CREATE:
+            createTable(command, collection);
+            break;
+        case INSERT:
+            insertRecord(command, collection, result);
+            break;
+        case DELETE:
+            deleteAllRecords();
+            break;
+        case SELECT:
+            performSelect(command, collection, result);
+            printSelectionResult(result);
+            break;
+        case UNKNOWN:
+            std::cout << "Unknown command." << std::endl;
+            break;
+        }
+    }
+
+    return 0;
+
+
+    /*
+    Приклад вводу команд:
+    CREATE cats id name age food
+    INSERT INTO cats 1 Snizhok 5 meat
+    SELECT FROM cats
+    SELECT FROM cats WHERE age > 5
+    SELECT FROM cats OBDER_BY age DESC
+    SELECT FROM cats ORDER_BY name ASC
+    */
+
+}
---
+++ balatska_fi-13_krivoshapova_fi-13_cp1/src/parser.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <string>
+#include <vector>
+
+enum CommandType {
+    CREATE,
+    INSERT,
+    SELECT,
+    DELETE,
+    UNKNOWN
+};
+
+struct Command {
+    CommandType type = UNKNOWN;
+    std::string table_name;
+    std::vector<std::string> columns;
+    std::vector<std::string> values;
+    std::string condition;
+    std::string order_by;
+    std::string order;
+    std::string select_all;
+    std::vector<std::vector<std::string>> data;
+    std::string operation;
+    std::string order_by_column;
+};
+
+struct SelectionResult {
+    std::vector<std::string> columns;
+    std::vector<std::vector<std::string>> data;
+};
+
+
+Command parseCommand(std::string input);
