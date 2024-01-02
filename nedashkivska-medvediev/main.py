62991b6bf22eddd0161608910d91167f665ad965
FI14ArinaNdk <arined-ipt25@lll.kpi.ua>
2024-01-02 17:39:15 +0200

    change

--- nedashkivska-medvediev/main.py
+++ nedashkivska-medvediev/main.py
@@ -1,87 +1,86 @@
-from myparser import parse_create, parse_insert, parse_select
-from database import Database
+from database import *
+import myparser
 
+def search_table(tables_dict: dict, name: str) -> int:
+    return tables_dict.get(name, -1)
 
-def parse_command(command):
-    tokens = []
-    current_token = ''
-    in_parentheses = False
+def handle_create_command(tables_dict, table_name, column_names):
+    if table_name not in tables_dict:
+        tables_dict[table_name] = table(table_name, column_names, [])
+        print(f"Таблицю успішно створено \"{table_name}\"")
+    else:
+        print("Таблиця з такою назвою вже існує")
 
-    for char in command:
-        if char in ('(', ')', ',', ';'):
-            if current_token.strip(): 
-                tokens.append(current_token.strip())
-                current_token = ''
-            tokens.append(char)
-            if char == '(':
-                in_parentheses = True
-            elif char == ')':
-                in_parentheses = False
-        elif char == ' ' and not in_parentheses:
-            if current_token.strip():  
-                tokens.append(current_token.strip())
-                current_token = ''
+def handle_insert_command(tables_dict, table_name, data):
+    table_obj = search_table(tables_dict, table_name)
+    if table_obj == -1:
+        print("Такої таблички не існує")
+    else:
+        if len(table_obj.columns) == len(data):
+            table_obj.new_row(data)
+            print(f"Успішно додано в \'{table_name}\'")
         else:
-            current_token += char
+            print(f"\'{table_name}\' має {len(table_obj.columns)} колонок, але ви мені надали {len(data)} аргумента")
 
-    if current_token.strip(): 
-        tokens.append(current_token.strip())
+def handle_select_all_from_command(tables_dict, table_name):
+    table_obj = search_table(tables_dict, table_name)
+    if table_obj == -1:
+        print("Такої таблички не існує")
+    else:
+        table_obj.print_table()
+def handle_select_from_command(tables_dict, table_name,col_name, data):
+    table_obj = search_table(tables_dict, table_name)
+    if table_obj == -1:
+        print("Такої таблички не існує")
+    else:
+        select(table_obj, col_name, data).print_table()
 
-    return tokens
-
-def handle_create(db, full_cmd):
-    tokens = parse_command(full_cmd)
-    try:
-        create_command = parse_create(tokens)
-        result = db.create_table(create_command)
-        print(result)
-    except ValueError as e:
-        print(e)
-
-def handle_insert(db, full_cmd):
-    tokens = parse_command(full_cmd)
-    try:
-        insert_command = parse_insert(tokens)
-        result = db.insert_into_table(insert_command)
-        print(result)
-    except ValueError as e:
-        print(e)
-
-def handle_select(db, full_cmd):
-    tokens = parse_command(full_cmd)
-    try:
-        select_command = parse_select(tokens)
-        result = db.select_from_table(select_command)
-        print(result)
-    except ValueError as e:
-        print(e)
-
-def main():
-    db = Database()
-
-    print("Example:")
-    print(" > create table (first_column, second_column, ....);")
-    print(" > insert table (\"1\", \"data\", ....);")
-    print(" > select from table where second_column < \"data\";")
-    print("------------------")
-
-    for cmd in iter(input, 'exit'):
-        buffer = [cmd]
-
-        if ';' in cmd:
-            full_cmd = ' '.join(buffer).strip()
-            
-            try:
-                if full_cmd.lower().startswith("create"):
-                    handle_create(db, full_cmd)
-                elif full_cmd.lower().startswith("insert"):
-                    handle_insert(db, full_cmd)
-                elif full_cmd.lower().startswith("select"):
-                    handle_select(db, full_cmd)
-                else:
-                    print("Error: Unknown or incorrect command")
-            except ValueError as e:
-                print(e)
 
 if __name__ == "__main__":
-    main()
+    tables_dict = {}
+    
+    print("Приклади команд:")
+    print(" > CREATE some_table (first_column, second_column);")
+    print(" > INSERT INTO some_table (\"1\", \"data\");")
+    print(" > SELECT FROM some_table WHERE second_column < \"data\";")
+    print("------------------")
+    
+    while True:
+        query = str(input("> "))
+        try:
+            result = myparser.parse(query)
+            command = result[0]
+            where_key=''
+            if len(result)>2:
+                where_key=result[2]            
+                       
+            if command == "exit":
+                break
+            
+            table_name = result[1]
+            
+            if command == "create":
+                column_names = result[2:]
+                handle_create_command(tables_dict, table_name, column_names)
+                
+            elif command == "insert into":
+                data = result[2:]
+                handle_insert_command(tables_dict, table_name, data)
+            
+            elif command == "insert":
+                data = result[2:]
+                handle_insert_command(tables_dict, table_name, data)
+                
+            elif command == "select from" and where_key!="where":
+                data = result[2:]
+                handle_select_all_from_command(tables_dict, table_name)
+
+            elif command == "select from" and where_key=="where":
+                data = result[2:]
+                handle_select_from_command(tables_dict, table_name,col_name=result[3], data=result[4])
+                
+            else:
+                print("Невідома команда")
+                
+        except Exception as e:
+            print(str(e))
